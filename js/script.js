76f303f4fb9299100853f5c3632a8298c5db6103
Iamfrontend <iamfrontend@mail.ru>
2017-10-06 14:55:15 +0500

    1

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	var windowHeight = window.innerHeight,
+		windowHeightPercent = windowHeight/1000;
+	//Трансформация схемы зала при изменении окна браузера
+	$(window).resize(function(){		
+		console.log(windowHeightPercent);
+		if(windowHeightPercent < 1){
+			$('.hall-scheme').css('transform', 'scale('+windowHeightPercent+')');
+		}
+	});	
+});
