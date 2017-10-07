0c0dcca2ea96474da9ddfa1b1355b72c59ce220e
FrontendHere <iamfrontend@mail.ru>
2017-10-07 13:20:18 +0500

    html end

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@ $(document).ready(function() {
 	var windowHeight = window.innerHeight,
 		windowHeightPercent = windowHeight/1000;
 	//Трансформация схемы зала при изменении окна браузера
-	$(window).resize(function(){		
+	/*$(window).resize(function(){		
 		console.log(windowHeightPercent);
 		if(windowHeightPercent < 1){
 			$('.hall-scheme').css('transform', 'scale('+windowHeightPercent+')');
 		}
-	});	
+	});*/	
 });
